679f84658ad91676612fdd18de890c99625c78be
Mark Linn <marktlinn@gmail.com>
2022-12-13 17:25:03 +0100

    add: delete router in server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,12 +1,12 @@
 const User = require("../models/User");
-const crypto = require("crypto");
+const crypto = require("bcrypt");
 
 const updateUser = async (req, res) => {
   if (req.body.userId === req.params.id || req.user.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await crypto.genSalt(10);
-        req.body.password = await crypto.Hash(req.body.password, salt);
+        req.body.password = await crypto.hash(req.body.password, salt);
       } catch (err) {
         return res.status(500).json(err.message);
       }
@@ -24,4 +24,19 @@ const updateUser = async (req, res) => {
   }
 };
 
-module.exports = { updateUser };
+const deleteUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    try {
+      const user = await User.findOneAndDelete(req.params.id);
+      res.status(200).json("User has been deleted");
+    } catch (err) {
+      res.status(500).json("Error deleteing user: ", err.message);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("Permission not granted to delete this account.");
+  }
+};
+
+module.exports = { updateUser, deleteUser };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const { updateUser } = require("../controllers/user");
+const { updateUser, deleteUser } = require("../controllers/user");
 //Get user
 router.get("/", (req, res) => {
   res.send("<h1>User homepage</h1>");
 });
 
 //Delete user
-
+router.delete("/delete/:id", deleteUser);
 //Update user
 router.put("/update/:id", updateUser);
 //Follow user

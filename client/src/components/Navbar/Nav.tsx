5c6b9f3995c1f3f142d7d058b32d8b4681b2a11d
Mark Linn <marktlinn@gmail.com>
2022-12-16 17:04:10 +0100

    initial setup of client side app

--- client/src/components/Navbar/Nav.tsx
+++ client/src/components/Navbar/Nav.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="nav-container">
+      <div>
+        <span className="logo">TownSquare</span>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,5 +1,4 @@
 import Nav from "../../components/Navbar/Nav";
-
 const Home = () => {
   return (
     <div>

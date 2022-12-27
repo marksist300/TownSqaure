45dfcd400c505eb4af113ec33cd88e0b79951b72
Mark Linn <marktlinn@gmail.com>
2022-12-27 17:24:58 +0100

    add: useRefs for form data in login and signup files

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,6 +1,14 @@
 import style from "./Login.module.scss";
+import React, { useRef } from "react";
 
 const Login = () => {
+  const email = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email.current?.value, password.current?.value);
+  };
+
   return (
     <main className={style.loginPage}>
       <div className={style.wrapper}>
@@ -12,16 +20,21 @@ const Login = () => {
           </p>
         </div>
         <div className={style.loginRight}>
-          <form className={style.loginForm}>
+          <form className={style.loginForm} onSubmit={handleSubmit}>
             <input
+              required
               className={style.loginInput}
               type="email"
               placeholder="Enter email here..."
+              ref={email}
             />
             <input
+              required
               className={style.loginInput}
               type="password"
               placeholder="Enter password here..."
+              minLength={6}
+              ref={password}
             />
             <button className={style.submitBtn}>Log in</button>
             <span className={style.forgottenPassword}>
--- client/src/pages/Signup/Signup.tsx
+++ client/src/pages/Signup/Signup.tsx
@@ -1,6 +1,22 @@
 import style from "./Signup.module.scss";
+import { HTMLInputTypeAttribute, useRef } from "react";
 
 const Signup = () => {
+  const firstName = useRef<HTMLInputElement>(null);
+  const lastName = useRef<HTMLInputElement>(null);
+  const email = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+  const passwordCheck = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password.current?.value !== passwordCheck.current?.value) {
+      return "Error message";
+    } else {
+      const username = `${firstName.current?.value} ${lastName.current?.value}`;
+    }
+  };
+
   return (
     <main className={style.signupPage}>
       <div className={style.wrapper}>
@@ -12,39 +28,52 @@ const Signup = () => {
           </p>
         </div>
         <div className={style.signupRight}>
-          <form className={style.signupForm}>
+          <form className={style.signupForm} onSubmit={handleSubmit}>
             <div className={style.namesSection}>
               <input
+                required
                 className={style.signupInput}
                 name="firstName"
                 type="text"
                 placeholder="First name"
+                ref={firstName}
               />
               <input
+                required
                 className={style.signupInput}
                 name="lastName"
                 type="text"
                 placeholder="Last name"
+                ref={lastName}
               />
             </div>
             <input
+              required
               className={style.signupInput}
               type="email"
               placeholder="Enter email"
+              ref={email}
             />
             <input
+              required
               className={style.signupInput}
               type="password"
               placeholder="Enter password"
+              minLength={6}
+              ref={password}
             />
             <input
+              required
               className={style.signupInput}
               type="password"
               placeholder="Re-enter password"
+              minLength={6}
+              ref={passwordCheck}
             />
             <button className={style.submitBtn}>Signup</button>
             <span className={style.goToAccount}>Already have an account?</span>
           </form>
+          {/*Output error message is passwords don't match*/}
         </div>
       </div>
     </main>

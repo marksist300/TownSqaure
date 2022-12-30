9875342c93e8e59cccd134db7ca88424c95dfb89
Mark Linn <marktlinn@gmail.com>
2022-12-30 20:38:30 +0100

    add: dummy data for development ease before implementing auth persistence

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,20 +1,15 @@
 import { createContext, ReactNode, useReducer } from "react";
+import { usersData } from "../dummyData";
 import AuthReducer from "./AuthReducer";
-
-const INITIAL_STATE = {
-  user: null,
-  isFetching: false,
-  error: false,
-};
 
 interface User {
   cover: string;
   email: string;
-  followers: string[];
-  following: string[];
+  followers?: string[];
+  following?: string[];
   isAdmin: boolean;
-  password: string;
-  profilePic: string;
+  password?: string;
+  profilePic?: string;
   username: string;
   _id: string;
 }
@@ -24,6 +19,32 @@ type AuthState = {
   isFetching: boolean;
   error: boolean;
   dispatch?: React.Dispatch<object>;
+};
+
+const dummyUser = {
+  _id: "63a5d21a31ac7abaf5e68a1d",
+  username: "Aaron Delta",
+  email: "aaron@email.com",
+  isAdmin: false,
+  profilePic:
+    "https://res.cloudinary.com/djucwl5l6/image/upload/v1671811130/pic8_akoaf5.jpg",
+  cover:
+    "https://res.cloudinary.com/djucwl5l6/image/upload/v1671811122/winter_qm3f32.png",
+  following: [
+    "63a5d106d7527e6b48d1b81f",
+    "63a5d26e31ac7abaf5e68a20",
+    "63a5d2dd31ac7abaf5e68a23",
+    "63a5d32731ac7abaf5e68a26",
+    "63a5d37831ac7abaf5e68a29",
+    "63a5d3c131ac7abaf5e68a2c",
+    "63a5d3f631ac7abaf5e68a2f",
+  ],
+};
+
+const INITIAL_STATE = {
+  user: dummyUser,
+  isFetching: false,
+  error: false,
 };
 
 export const AuthContext = createContext<AuthState>(INITIAL_STATE);

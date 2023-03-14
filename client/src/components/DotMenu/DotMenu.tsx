48de12ff644679bab702c7d7594f1cbf8d57ab2a
Mark Linn <marktlinn@gmail.com>
2023-03-14 15:30:39 +0100

    add: dotmenu focus trap for accessibility delete functionality working

--- client/src/components/DotMenu/DotMenu.tsx
+++ client/src/components/DotMenu/DotMenu.tsx
@@ -12,18 +12,67 @@ const DotMenu = ({ postId }: { postId: string }) => {
   const [deletePost] = useDeletePostMutation();
 
   const [displayMenu, setDisplayMenu] = useState(false);
-  const clickRef = useRef<HTMLInputElement>(null);
+  const clickRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<any>(null);
   useEffect(() => {
     function handleClickOutside(e: MouseEvent) {
       if (clickRef.current && !clickRef.current.contains(e.target as Node)) {
         setDisplayMenu(false);
-      }
+      } else return;
     }
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [clickRef]);
+
+  useEffect(() => {
+    if (displayMenu === true) {
+      function handleEscKey(e: KeyboardEvent) {
+        console.log(e.code);
+        if (e.code === "Escape") {
+          setDisplayMenu(false);
+        } else if (e.code === "Tab" || e.key === "Shift") {
+          if (e.code !== "Tab") return;
+          if (clickRef.current) {
+            const elements = Array.from(
+              clickRef.current.querySelectorAll("li")
+            );
+            const firstElement = elements[0];
+            const lastElement = elements[elements.length - 1];
+
+            if (e.shiftKey) {
+              if (
+                //Shift key down cycle backwards through all tab elements in open modal
+                !menuRef.current.contains(document.activeElement) ||
+                document.activeElement === firstElement
+              ) {
+                lastElement.focus();
+                e.preventDefault();
+              }
+            } else if (
+              //Shift ket not down, cycle through all tab elements in open modal.
+              !menuRef.current.contains(document.activeElement) ||
+              document.activeElement === lastElement
+            ) {
+              firstElement.focus();
+              e.preventDefault();
+            }
+          }
+        } else if (e.code === "Enter") {
+          const name = document.activeElement?.id;
+          const targetEl = menuRef.current.querySelector(`#${name}`);
+          targetEl.click();
+        }
+      }
+
+      document.addEventListener("keydown", handleEscKey);
+
+      return () => {
+        document.removeEventListener("keydown", handleEscKey);
+      };
+    }
+  }, [displayMenu]);
 
   const handleClick = async (
     e: React.MouseEvent<HTMLLIElement, MouseEvent>,
@@ -56,17 +105,23 @@ const DotMenu = ({ postId }: { postId: string }) => {
 
   return (
     <div className={style.dropdown} ref={clickRef}>
-      <button className={style.dropdownLink}>
-        <MoreVert onClick={() => setDisplayMenu(prev => !prev)} />
+      <button
+        className={style.dropdownLink}
+        onClick={() => setDisplayMenu(prev => !prev)}
+      >
+        <MoreVert />
       </button>
+      {/*Menu OPEN */}
       <div
         className={
           displayMenu ? style.dropdownMenuActive : style.dropdownMenuInactive
         }
         id="menu"
       >
-        <ul className={style.dropdownList}>
+        <ul className={style.dropdownList} ref={menuRef}>
           <li
+            tabIndex={1}
+            id="delete"
             onClick={e => {
               handleClick(e, "delete");
             }}
@@ -74,6 +129,8 @@ const DotMenu = ({ postId }: { postId: string }) => {
             Delete
           </li>
           <li
+            tabIndex={2}
+            id="edit"
             onClick={e => {
               handleClick(e, "edit");
             }}

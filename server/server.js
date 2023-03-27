d4ce39c9fafaaa002f9b50fc38003fe20761e027
Mark Linn <marktlinn@gmail.com>
2023-03-27 23:00:47 +0200

    tweak: cors on BE hardcode origin

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectionToMongoDB();
 //middleware to control CORS
 app.use(
   cors({
-    origin: "*",
+    origin: "https://townsquareapp.netlify.app",
     credentials: true,
     methods: ["GET", "PUT", "POST", "DELETE"],
   })

de0f959712d710c1182668b6bf357912c80d2e56
Mark Linn <marktlinn@gmail.com>
2023-03-27 14:59:14 +0200

    add: cors support to apiSlice

--- client/src/app/apiSlice.tsx
+++ client/src/app/apiSlice.tsx
@@ -3,6 +3,7 @@ import { RootState } from "./store";
 
 const TSBaseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_SERVER_DOMAIN,
+  mode: "cors",
   prepareHeaders: (headers, { getState }) => {
     // By default, if we have a token in the store, let's use that for authenticated requests
     const token = (getState() as RootState).auth.token;

769d1c952865771869da2b55299ae46ed1a54945
Mark Linn <marktlinn@gmail.com>
2023-03-27 23:06:42 +0200

    tweak: apislice credentials

--- client/src/app/apiSlice.tsx
+++ client/src/app/apiSlice.tsx
@@ -4,7 +4,6 @@ import { RootState } from "./store";
 const TSBaseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_SERVER_DOMAIN,
   mode: "cors",
-  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     // By default, if we have a token in the store, let's use that for authenticated requests
     const token = (getState() as RootState).auth.token;
